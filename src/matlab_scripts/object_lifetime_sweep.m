f2c14a270a898860ee607dee2c7cab68796c99c2
John Gilmore <jgilmore@ml.sun.ac.za>
2012-06-13 17:45:16 +0200

    Added many figures for MMVE 2012 paper and adding changes to the paper itself. This includes a mostly fully described model and all model results.

--- src/matlab_scripts/object_lifetime_sweep.m
+++ src/matlab_scripts/object_lifetime_sweep.m
@@ -8,17 +8,17 @@ type = 'exponential';
 R = 10;
 
 %The maximum expected network size
-N = 100;
+N = 120;
 
 %The maximum average group size
 G = 50;
 
 %Repair rate
-MU = 0.05;
-mu_step = 0.005;
+MU = 1/180;
+mu_step = 1/1800;
 
 %Time to live
-TTL = 300;
+TTL = 86400;
 
 %Parameters of a pareto node lifetime distribution (these values are
 %ignored if the specified distribution is exponential)
@@ -27,14 +27,14 @@ TTL = 300;
 
 %Parameter of an exponential distribution (these values are ignored
 %if the specified distribution is pareto)
-lambda = 100.7427
+lambda = 1/1800
 
 %Peer departure rate for a pareto distribution with parameters alpha and
 %beta
 if strcmp(type, 'pareto')
     theta = (alpha-2)/beta;
 else strcmp(type, 'exponential')
-    theta = 1/lambda;
+    theta = lambda;
 end
 
 lifetimes = zeros(N, R, G, MU/mu_step);
@@ -66,10 +66,9 @@ end
 
 figure
 surf(squeeze(lifetimes(:, R, :, MU/mu_step)));
-xlabel('Initial group size');
-ylabel('Average group size');
+xlabel('Average group size');
+ylabel('Initial group size');
 zlabel('Expected object lifetime');
-title('Surface plot of expected object lifetimes as functions of initial group size and average group size.');
 
 figure
 scatter3(lifetime_slice(:,1), lifetime_slice(:,2), lifetime_slice(:,3));
