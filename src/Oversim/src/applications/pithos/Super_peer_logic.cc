f3e6aa4a86f9c99cae703b35a3613576d019751d
John Gilmore <jgilmore@ml.sun.ac.za>
2012-08-13 00:08:38 +0200

    Did some coding in the lab. Think I've redone some stuf at home now. And now for merging things I'd already done. Fun.

--- src/Oversim/src/applications/pithos/Super_peer_logic.cc
+++ src/Oversim/src/applications/pithos/Super_peer_logic.cc
@@ -36,14 +36,8 @@ void Super_peer_logic::initialize()
 	directory_port = par("directory_port");
 
 	//Initialise queue statistics collection
-	OverlayWriteSignal = registerSignal("OverlayWrite");
-	groupSizeSignal = registerSignal("GroupSize");
-	OverlayDeliveredSignal =  registerSignal("OverlayDelivered");
-	joinTimeSignal = registerSignal("JoinTime");
-	storeNumberSignal  = registerSignal("StoreNumber");
-	overlayNumberSignal = registerSignal("OverlayNumber");
-
-	overlaysStoreFailSignal =  registerSignal("overlaysStoreFail");
+	//groupSizeSignal = registerSignal("GroupSize");
+	//joinTimeSignal = registerSignal("JoinTime");
 
 	cModule *groupLedgerModule = getParentModule()->getSubmodule("sp_group_ledger");
 	group_ledger = check_and_cast<GroupLedger *>(groupLedgerModule);
@@ -83,7 +77,7 @@ void Super_peer_logic::initialize()
 
 void Super_peer_logic::finish()
 {
-	cModule *communicatorModule = getParentModule()->getSubmodule("communicator");
+	/*cModule *communicatorModule = getParentModule()->getSubmodule("communicator");
 	Communicator *communicator = check_and_cast<Communicator *>(communicatorModule);
 
     simtime_t time = globalStatistics->calcMeasuredLifetime(communicator->getCreationTime());
@@ -99,7 +93,7 @@ void Super_peer_logic::finish()
         // record scalar data
     	globalStatistics->addStdDev((group_name.str() + std::string("Number of peer arrivals")).c_str() , numPeerArrivals);
     	globalStatistics->addStdDev((group_name.str() + std::string("Number of peer departures")).c_str() , numPeerDepartures);
-    }
+    }*/
 }
 
 void Super_peer_logic::handleOverlayWrite(cMessage *msg)
@@ -126,7 +120,7 @@ void Super_peer_logic::handleOverlayWrite(cMessage *msg)
 	}
 	delete(go);
 
-	emit(OverlayWriteSignal, 1);
+	//emit(OverlayWriteSignal, 1);
 
 	EV << "Packet sent for storage in the overlay\n";
 }
@@ -271,7 +265,7 @@ void Super_peer_logic::addSuperPeer()
 
 	send(boot_p, "comms_gate$o");
 
-	emit(joinTimeSignal, simTime());
+	//emit(joinTimeSignal, simTime());
 
 	//TODO: Add resend or timer that checks whether the join request has been handled by the Directory.
 }
@@ -411,7 +405,7 @@ void Super_peer_logic::handlePeerLeaving(PeerData peer_data)
 	//Inform the last peer that joined of the last peer that left, in case the peer that left did not know about the peer that joined.
 	informLastJoinedOfLastLeft();
 
-	emit(groupSizeSignal, group_ledger->getGroupSize());
+	//emit(groupSizeSignal, group_ledger->getGroupSize());
 }
 
 void Super_peer_logic::handleMessage(cMessage *msg)
@@ -467,7 +461,7 @@ void Super_peer_logic::handleMessage(cMessage *msg)
 		{
 			handleJoinReq(msg);
 
-			emit(groupSizeSignal, group_ledger->getGroupSize());
+			//emit(groupSizeSignal, group_ledger->getGroupSize());
 		} else error("Super peer received unknown group message from communicator");
 		delete(msg);
 	} else {
