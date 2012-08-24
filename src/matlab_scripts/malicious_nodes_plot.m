e62786449411a82103d6e61a2b659e2459f5b0d9
John Gilmore <jgilmore@ml.sun.ac.za>
2012-08-24 22:20:03 +0200

    Added malicious node description to evaluation and updated Matlab script.

--- src/matlab_scripts/malicious_nodes_plot.m
+++ src/matlab_scripts/malicious_nodes_plot.m
@@ -2,15 +2,17 @@ close all
 clear all
 
 % Retrieval method & Malicious nodes&Reliability&Responsiveness&Group bandwidth&Overlay bandwidth  \\
-%                  &                &           &      (s)     & in/out (Bps)  &  in/out (Bps)     \\
-%     fast         & 0.125          &           &              &               &                    \\
+%     fast         & 0.0            &  0.9970   &      (s)     & in/out (Bps)  &  in/out (Bps)     \\
+%     fast         & 0.125          &  0.8767   &              &               &                    \\
 %     fast         & 0.25           &  0.7482   &    0.198     &  186/182      &    1186/1199      \\
-%     fast         & 0.375          &           &              &               &                    \\
+%     fast         & 0.375          &  0.6248   &              &               &                    \\
 %     fast         & 0.5            &  0.5025   &    0.192     &  177/173      &    1180/1194      \\
-%     fast         & 0.125          &           &              &               &                    \\
+%     fast         & 0.625          &  0.3722   &              &               &                    \\
 %     fast         & 0.75           &  0.2669   &    0.191     &  179/175      &    1179/1192      \\
-%     fast         & 0.125          &           &              &               &                    \\
+%     fast         & 0.875          &  0.1324   &              &               &                    \\
+%     fast         & 1.0            &  0.0061   &              &               &                    \\
 
+%     safe         & 0.0            &  0.9998
 %     safe         & 0.125          &  0.9926   &    0.351     &  724/590      &   1187/1201  \\
 %     safe         & 0.25           &  0.9558   &    0.356     &  721/588      &              \\
 %     safe         & 0.375          &  0.8609   &    0.295     &  714/583      &   979/993    \\
@@ -22,11 +24,10 @@ clear all
 
 safe_get_rel = [0.9998, 0.9926 , 0.9558 , 0.8609 , 0.7134 , 0.5047 , 0.3042 , 0.1135 , 0.0];
 
-fast_get_rel = [0.9970, 0.7482, 0.5025 , 0.2669 , 0.0];
+fast_get_rel = [0.9970, 0.8767, 0.7482, 0.6248, 0.5025 , 0.3722, 0.2669, 0.1324, 0.0061];
 
-malicious_nodes_fast = [0.0, 0.25, 0.5, 0.75, 1.0];
-malicious_nodes_safe = [0.0, 0.125 , 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0];
+malicious_nodes = [0.0, 0.125 , 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0];
 
-plot(malicious_nodes_safe, safe_get_rel, 'ko-');
+plot(malicious_nodes, safe_get_rel, 'ko-');
 hold on
-plot(malicious_nodes_fast, fast_get_rel, 'ko-');
+plot(malicious_nodes, fast_get_rel, 'ko-');
