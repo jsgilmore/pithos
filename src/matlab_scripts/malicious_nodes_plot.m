1a426fe2785621d2b63aaa85073c3d15f57a4fe1
John Gilmore <jgilmore@ml.sun.ac.za>
2012-09-25 15:19:13 +0200

    Completed evaluation chapter second draft.

--- src/matlab_scripts/malicious_nodes_plot.m
+++ src/matlab_scripts/malicious_nodes_plot.m
@@ -10,10 +10,10 @@ fast_get_rel =         [0.9970,   0.8767,   0.7482,   0.6248,   0.5025,   0.3722
 overlay_get_rel =      [0.9140,   0.8557,   0.7056,   0.5156,   0.3437,   0.1921,   0.0797,   0.0166,   0.0];
 
 %Responsiveness
-safe4_get_resp =       [0.354,    0.351,    0.356,    0.295,    0.346,    0.350,    0.350,    0.354,    0.356];
-safe6_get_resp =       [0.788,    0.789,    0.793,    0.637,    0.785,    0.780,    0.778,    0.789,    0.800];
+safe4_get_resp =       [0.354,    0.351,    0.356,    0.353,    0.346,    0.350,    0.350,    0.354,    0.356];
+safe6_get_resp =       [0.788,    0.789,    0.793,    0.789,    0.785,    0.780,    0.778,    0.789,    0.800];
 fast_get_resp =        [0.196,    0.197,    0.197,    0.203,    0.217,    0.213,    0.220,    0.218,    0.165];
-overlay_get_resp =     [1.760,    1.805,    1.841,    1.825,    1.785,    1.758,    1.692,    1.680,    0.0  ];
+overlay_get_resp =     [1.760,    1.805,    1.841,    1.825,    1.785,    1.758,    1.692,    1.680,    1.680];
 
 %Data used
 safe4_get_data_in =    [4,        4,        4,        4,        4,        4,        4,        4,        4];
@@ -40,13 +40,13 @@ overlay_get_group_out= [0  ,      0  ,      0  ,      0  ,      0  ,      0  ,  
 %Overlay bandwidth
 safe4_get_overlay_in=  [1187,     1187,     1192,     1187,     1185,     1188,     1188,     1188,     1183];
 safe6_get_overlay_in=  [1186,     1187,     1187,     1184,     1186,     1188,     1188,     1188,     1183];
-fast_get_overlay_in =  [1180,     1183,     1182,     1185      1183,     1173      1174,     1162,     0   ];
-overlay_get_overlay_in=[1180,     1183,     1182,     1185,     1183,     1173,     1174,     1161,     0   ];
+fast_get_overlay_in =  [1180,     1183,     1182,     1185      1183,     1173      1174,     1161,     1160];
+overlay_get_overlay_in=[1180,     1183,     1182,     1185,     1183,     1173,     1174,     1161,     1160];
 
-safe4_get_overlay_out= [1201,     1201,     1208,     1203,      1199,     1202,     1203,     1205,     1197];
-safe6_get_overlay_out= [1200,     1201,     1202,     1199,      1200,     1202,     1202,     1205,     1197];
-fast_get_overlay_out = [1192,     1196,     1193,     1197,     1197,     1186,     1187,     1176,     0   ];
-overlay_get_overlay_out=[1182,    1196,     1193,     1197,     1197,     1186,     1187,     1176,     0   ];
+safe4_get_overlay_out= [1201,     1201,     1208,     1203,      1199,     1202,     1203,     1205,    1197];
+safe6_get_overlay_out= [1200,     1201,     1202,     1199,      1200,     1202,     1202,     1205,    1197];
+fast_get_overlay_out = [1192,     1196,     1193,     1197,     1197,     1186,     1187,     1176,     1176];
+overlay_get_overlay_out=[1182,    1196,     1193,     1197,     1197,     1186,     1187,     1176,     1176];
 
 safe4_get_in = safe4_get_group_in + safe4_get_overlay_in;
 safe6_get_in = safe6_get_group_in + safe6_get_overlay_in;
@@ -74,7 +74,7 @@ plot(malicious_nodes, fast_get_rel, 'ko-', 'LineWidth',2);
 hold on
 plot(malicious_nodes, safe4_get_rel, 'rs-', 'LineWidth',2);
 plot(malicious_nodes, safe6_get_rel, 'bd-', 'LineWidth',2);
-plot(malicious_nodes, overlay_get_rel, '+-', 'Color',[0.17,0.51,0.34], 'LineWidth',2);
+plot(malicious_nodes, overlay_get_rel, '+-', 'Color',[0.17,0.51,0.34],'MarkerSize',12, 'LineWidth',2);
 ylabel('Reliability');
 xlabel('Malicious node probability');
 hleg = legend('Fast retrieval','Safe retrieval, 4 compares','Safe retrieval, 6 compares', 'Overlay retrieval');
@@ -86,7 +86,7 @@ plot(malicious_nodes, fast_get_resp, 'ko-', 'LineWidth',2);
 hold on
 plot(malicious_nodes, safe4_get_resp, 'rs-', 'LineWidth',2);
 plot(malicious_nodes, safe6_get_resp, 'bd-', 'LineWidth',2);
-plot(malicious_nodes, overlay_get_resp, '+-', 'Color',[0.17,0.51,0.34], 'LineWidth',2);
+plot(malicious_nodes, overlay_get_resp, '+-', 'Color',[0.17,0.51,0.34],'MarkerSize',12, 'LineWidth',2);
 ylabel('Latency (s)');
 xlabel('Malicious node probability');
 hleg = legend('Fast retrieval','Safe retrieval, 4 compares','Safe retrieval, 6 compares', 'Overlay retrieval');
@@ -98,8 +98,9 @@ plot(malicious_nodes, fast_get, 'ko-', 'LineWidth',2);
 hold on
 plot(malicious_nodes, safe4_get, 'rs-', 'LineWidth',2);
 plot(malicious_nodes, safe6_get, 'bd-', 'LineWidth',2);
-plot(malicious_nodes, overlay_get, '+-', 'Color',[0.17,0.51,0.34], 'LineWidth',2);
+plot(malicious_nodes, overlay_get, '+-', 'Color',[0.17,0.51,0.34],'MarkerSize',12, 'LineWidth',2);
+plot(malicious_nodes, fast_get_data, 'd-', 'Color',[0.48,0.06,0.89], 'LineWidth',2);
 ylabel('Bandwidth (Bps)');
 xlabel('Malicious node probability');
-hleg = legend('Fast retrieval','Safe retrieval, 4 compares','Safe retrieval, 6 compares', 'Overlay retrieval');
+hleg = legend('Fast retrieval','Safe retrieval, 4 compares','Safe retrieval, 6 compares', 'Overlay retrieval', 'Higher layer');
 set(hleg, 'Location', 'SouthWest');
--- src/matlab_scripts/process_responsiveness.m
+++ src/matlab_scripts/process_responsiveness.m
@@ -1,18 +1,18 @@
 close all
-clear all
-
-folder = '../results/LAN_responsiveness/';
-
-eval(['load ' folder 'group_get.csv']);
-eval(['load ' folder 'group_put.csv']);
-eval(['load ' folder 'overlay_get.csv']);
-eval(['load ' folder 'overlay_put.csv']);
-eval(['load ' folder 'overall_get.csv']);
-eval(['load ' folder 'overall_put.csv']);
+% clear all
+% 
+% folder = '../results/LAN_responsiveness/';
+% 
+% eval(['load ' folder 'group_get.csv']);
+% eval(['load ' folder 'group_put.csv']);
+% eval(['load ' folder 'overlay_get.csv']);
+% eval(['load ' folder 'overlay_put.csv']);
+% eval(['load ' folder 'overall_get.csv']);
+% eval(['load ' folder 'overall_put.csv']);
 
 figure('Name','group get')
 set(gca, 'FontSize', 24) 
-hist(group_get(:,2), 5000);
+hist(group_get(:,2), 500000);
 h = findobj(gca,'Type','patch');
 set(h,'FaceColor','k','EdgeColor','k')
 ylabel('Number of messages');
